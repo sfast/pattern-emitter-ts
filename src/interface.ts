cfd685dac8e03b9b5ced215ada83eb31537c97d8
Artak Vardanyan <artakvg@gmail.com>
2019-04-30 19:57:16 +0400

    typedoc fix

---
+++ src/interface.ts
@@ -0,0 +1,37 @@
+/**
+ * @module IPatternEmitter
+ */
+
+import {EventEmitter} from "events";
+
+import { PatternEmitter } from "./patternEmitter";
+
+import {
+    EventEmitterType,
+    EventPattern,
+    PatternListener,
+    PatternEmitterInterfaceFunction,
+} from "./types";
+
+
+export interface IPatternEmitter {
+    addListener: PatternEmitterInterfaceFunction;
+    removeListener: PatternEmitterInterfaceFunction;
+    on: PatternEmitterInterfaceFunction;
+    once: PatternEmitterInterfaceFunction;
+    off: PatternEmitterInterfaceFunction;
+    // @todo prependListener: PatternEmitterInterfaceFunction;
+    // @todo prependOnceListener: PatternEmitterInterfaceFunction;
+
+    removeAllListeners(type?: EventPattern): PatternEmitter | EventEmitter;
+    listenerCount(type: EventPattern): number;
+
+    // @todo setMaxListeners(n: number): PatternEmitter | EventEmitter;
+    // getMaxListeners(): number;
+    listeners(event: EventEmitterType): PatternListener[];
+    // @todo rawListeners(event: EventEmitterType): Function[];
+    emit(event: EventEmitterType, ...args: any[]): boolean;
+
+    // @todo eventNames(): Array<EventEmitterType>;
+    // @todo eventPatterns(): Array<EventPattern>;
+}
--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,18 @@
 /**
- * @module PatternEmitter Types
+ * @module Types
  */
+
+import {EventEmitter} from "events";
+import { PatternEmitter } from "./patternEmitter";
+
+export type EventEmitterType = string | symbol;
+export type EventPattern = RegExp | EventEmitterType;
+
+export type PatternListener = (...args: any[]) => void;
+
+
+export type EventInterfaceFunction = (event: string | symbol , listener: PatternListener) =>  EventEmitter;
+export type PatternEventInterfaceFunction =  (event: EventPattern, listener: PatternListener) => PatternEmitter | EventEmitter;
+
+export type EventEmitterInterfaceFunction = EventInterfaceFunction;
+export type PatternEmitterInterfaceFunction =  PatternEventInterfaceFunction;
